316cfa85accada42263f5eaab0748700792c56a9
sa-wa-m <fwvillage@gmail.com>
2016-03-22 08:11:55 +0900

    Fix cpp setting (in progress)

--- .emacs.d/inits/06-cpp.el
+++ .emacs.d/inits/06-cpp.el
@@ -19,6 +19,7 @@
 ;; ---- fly checker sett>ing ---
 (add-hook 'c-mode-hook 'flycheck-mode)
 (add-hook 'c++-mode-hook 'flycheck-mode)
+(add-hook 'c++-mode-hook      'irony-mode)
 
 (defmacro flycheck-define-clike-checker (name command modes)
   `(flycheck-define-checker ,(intern (format "%s" name))
@@ -97,3 +98,54 @@
 (define-key ggtags-mode-map (kbd "C-c g u") 'ggtags-update-tags)
 
 (define-key ggtags-mode-map (kbd "M-,") 'pop-tag-mark)
+
+;; (use-package flycheck
+;;   :config
+;;   (progn
+;;     (add-hook 'flycheck-mode-hook #'flycheck-irony-setup)
+;;     )
+;;   )
+
+;; (use-package cmake-ide
+;;   :bind
+;;   (("<f9>" . cmake-ide-compile))
+;;   :config
+;;   (progn
+;;     (setq 
+;;      ; rdm & rcコマンドへのパス。コマンドはRTagsのインストール・ディレクトリ下。
+;;      cmake-ide-rdm-executable "path_to_rdm"
+;;      cmake-ide-rc-executable  "path_to_rc"
+;;      )
+;;     )
+;;   )
+
+;; (use-package rtags
+;;   :config
+;;   (progn
+;;     (rtags-enable-standard-keybindings c-mode-base-map)
+;;     ; 関数cmake-ide-setupを呼ぶのはrtagsをrequireしてから。
+;;     (cmake-ide-setup)
+;;     )
+;;   )
+
+(defun my-irony-mode-hook ()
+  (define-key irony-mode-map
+    [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map
+    [remap complete-symbol]
+    'irony-completion-at-point-async)
+  )
+
+(use-package irony
+  :config
+  (progn
+    ; ironyのビルド&インストール時にCMAKE_INSTALL_PREFIXで指定したディレクトリへのパス。
+    (setq irony-server-install-prefix "where_to_install_irony")
+    (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
+    (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+    (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+    (add-hook 'irony-mode-hook 'irony-eldoc)
+    (add-to-list 'company-backends 'company-irony)
+    )
+  )
