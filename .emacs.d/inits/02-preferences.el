1d4367e65ead9b60100d745f0cb0ce0411d2cd42
Hiroyuki Funaya <hiroyuki_funaya@goyota.co.jp>
2016-03-27 11:13:47 +0900

    Color theme changed and installed

--- .emacs.d/inits/02-preferences.el
+++ .emacs.d/inits/02-preferences.el
@@ -1,21 +1,28 @@
+;; color-theme
+;; Buffer-dependent color themes
+
+(require 'color-theme)
+(color-theme-initialize)
+(color-theme-aliceblue)
+
 ;; font setting
-(if (mac?)
-(progn
-(set-face-attribute 'default nil
-		    :family "Ricty Diminished"
-		    :height 140)
-(set-fontset-font "fontset-default"
-		  'japanese-jisx0208
-		  '("Ricty Diminished" . "iso10646-1"))
-(set-fontset-font "fontset-default"
-		  'katakana-jisx0201
-		  '("Ricty Diminished" . "iso10646-1"))
-(setq face-font-rescale-alist
-      '((".*Ricty-bold.*" . 1.0)
-	(".*Ricty-medium.*" . 1.0)
-	(".*HiraKakuProN-W6.*" . 1.2)
-	(".*Osaka-medium.*" . 1.2)
-	("-cdac$" . 1.4)))
-)
-(add-to-list 'default-frame-alist '(font . "ricty-16"))
-)
+;; (if (mac?)
+;; (progn
+;; (set-face-attribute 'default nil
+;; 		    :family "Ricty Diminished"
+;; 		    :height 140)
+;; (set-fontset-font "fontset-default"
+;; 		  'japanese-jisx0208
+;; 		  '("Ricty Diminished" . "iso10646-1"))
+;; (set-fontset-font "fontset-default"
+;; 		  'katakana-jisx0201
+;; 		  '("Ricty Diminished" . "iso10646-1"))
+;; (setq face-font-rescale-alist
+;;       '((".*Ricty-bold.*" . 1.0)
+;; 	(".*Ricty-medium.*" . 1.0)
+;; 	(".*HiraKakuProN-W6.*" . 1.2)
+;; 	(".*Osaka-medium.*" . 1.2)
+;; 	("-cdac$" . 1.4)))
+;; )
+;; (add-to-list 'default-frame-alist '(font . "ricty-16"))
+;; )
